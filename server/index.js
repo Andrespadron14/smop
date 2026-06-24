350e2a3ecf57f2972a14616d382cbc372ead1b29
Andrespadron14 <andrespadron14@users.noreply.github.com>
2026-06-24 01:01:51 -0400

    Force redeploy for NODE_OPTIONS DNS fix

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.get('*', (req, res) => {
 
 async function start() {
   try {
-    console.log('SMOP starting...');
+    console.log('SMOP iniciando...');
     await initDb();
     const { seed } = require('./seed');
     await seed();
